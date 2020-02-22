ec232c195198e9cc234207b80f65696da1bedb13
Micah Wood <micah@wpscholar.com>
2020-02-22 18:10:00 -0500

    Try to do real stuff now.

---
+++ .github/actions/github-event-data/index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+  // Get payload
+  const payload = github.context.payload;
+
+  const repositoryName = payload.repository.name.toLowerCase();
+  core.setOutput('repositoryName', repositoryName);
+
+  const ownerName = payload.repository.owner.name.toLowerCase();
+  core.setOutput('ownerName', ownerName);
+
+  const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
+  core.setOutput('isBranch', isBranch);
+
+  const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');
+  core.setOutput('isTag', isTag);
+
+  const isRelease = payload.hasOwnProperty('release');
+  core.setOutput('isRelease', isRelease);
+
+  const branchName = isBranch ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
+  core.setOutput('branchName', branchName);
+
+  const tagName = isTag ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
+  core.setOutput('tagName', tagName);
+
+  const releaseId = isRelease ? payload.release.id : null;
+  core.setOutput('releaseId', releaseId);
+
+} catch(error) {
+
+  core.setFailed(error.message);
+
+}
