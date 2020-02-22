5ba80e60d6b24d09690ffd2b56ad60fc3fd53eab
Micah Wood <micah@wpscholar.com>
2020-02-22 17:42:02 -0500

    A bit more

--- .github/actions/get-event-payload/index.js
+++ .github/actions/get-event-payload/index.js
@@ -8,12 +8,12 @@ try {
 
   console.log(payload);
 
-  /*const repositoryName = payload.repository.name.toLowerCase();
+  const repositoryName = payload.repository.name.toLowerCase();
   const ownerName = payload.repository.owner.name.toLowerCase();
 
   console.log(repositoryName, ownerName);
 
-  const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
+  /*const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
   const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');
   const isRelease = payload.hasOwnProperty('release');
 
@@ -21,19 +21,19 @@ try {
 
 //    const branchName = isBranch ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
 //    const tagName = isTag ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
-  const releaseId = isRelease ? payload.release.id : null;
+  const releaseId = isRelease ? payload.release.id : null;*/
 
 //    console.log(branchName, tagName, releaseId);
 
   core.setOutput('payload', payload);
   core.setOutput('repositoryName', repositoryName);
   core.setOutput('ownerName', ownerName);
-  core.setOutput('isBranch', isBranch);
+  /*core.setOutput('isBranch', isBranch);
   core.setOutput('isTag', isTag);
-  core.setOutput('isRelease', isRelease);
+  core.setOutput('isRelease', isRelease);*/
 //    core.setOutput('branchName', branchName);
 //    core.setOutput('tagName', tagName);
-  core.setOutput('releaseId', releaseId);*/
+//  core.setOutput('releaseId', releaseId);
 
 } catch(error) {
 
