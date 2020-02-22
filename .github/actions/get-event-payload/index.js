de00bcf83848ac2864fe7b3c4f3fa4ad830b549e
Micah Wood <micah@wpscholar.com>
2020-02-22 17:40:52 -0500

    Just try the payload

--- .github/actions/get-event-payload/index.js
+++ .github/actions/get-event-payload/index.js
@@ -1,48 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function run() {
+try {
 
-  try {
+  // Get payload
+  const payload = github.context.payload;
 
-    // Get payload
-    const payload = github.context.payload;
+  console.log(payload);
 
-    console.log(payload);
+  /*const repositoryName = payload.repository.name.toLowerCase();
+  const ownerName = payload.repository.owner.name.toLowerCase();
 
-    const repositoryName = payload.repository.name.toLowerCase();
-    const ownerName = payload.repository.owner.name.toLowerCase();
+  console.log(repositoryName, ownerName);
 
-    console.log(repositoryName, ownerName);
+  const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
+  const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');
+  const isRelease = payload.hasOwnProperty('release');
 
-    const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
-    const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');
-    const isRelease = payload.hasOwnProperty('release');
-
-    console.log(isBranch, isTag, isRelease);
+  console.log(isBranch, isTag, isRelease);
 
 //    const branchName = isBranch ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
 //    const tagName = isTag ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : null;
-    const releaseId = isRelease ? payload.release.id : null;
+  const releaseId = isRelease ? payload.release.id : null;
 
 //    console.log(branchName, tagName, releaseId);
 
-    core.setOutput('payload', payload);
-    core.setOutput('repositoryName', repositoryName);
-    core.setOutput('ownerName', ownerName);
-    core.setOutput('isBranch', isBranch);
-    core.setOutput('isTag', isTag);
-    core.setOutput('isRelease', isRelease);
+  core.setOutput('payload', payload);
+  core.setOutput('repositoryName', repositoryName);
+  core.setOutput('ownerName', ownerName);
+  core.setOutput('isBranch', isBranch);
+  core.setOutput('isTag', isTag);
+  core.setOutput('isRelease', isRelease);
 //    core.setOutput('branchName', branchName);
 //    core.setOutput('tagName', tagName);
-    core.setOutput('releaseId', releaseId);
+  core.setOutput('releaseId', releaseId);*/
 
-  } catch(error) {
+} catch(error) {
 
-    core.setFailed(error.message);
-
-  }
+  core.setFailed(error.message);
 
 }
-
-run();
