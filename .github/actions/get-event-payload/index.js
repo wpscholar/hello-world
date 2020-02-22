df6aa54883b63037cb01a1b226f10e019513cb94
Micah Wood <micah@wpscholar.com>
2020-02-22 17:02:48 -0500

    More tests

---
+++ .github/actions/get-event-payload/index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+  // Get payload
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  const repositoryName = payload.repository.name.toLowerCase();
+  const ownerName = payload.repository.owner.name.toLowerCase();
+
+  const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
+  const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tag');
+  const isRelease = payload.hasOwnProperty('release');
+
+  const branchName = isBranch ? payload.ref.replace('refs/heads/', '') : null;
+  const tagName = isTag ? payload.ref.replace('refs/tags/', '') : null;
+  const releaseId = isRelease ? payload.release.id : null;
+
+  core.setOutput('payload', payload);
+  core.setOutput('repositoryName', repositoryName);
+  core.setOutput('ownerName', ownerName);
+  core.setOutput('isBranch', isBranch);
+  core.setOutput('isTag', isTag);
+  core.setOutput('isRelease', isRelease);
+  core.setOutput('branchName', branchName);
+  core.setOutput('tagName', tagName);
+  core.setOutput('releaseId', releaseId);
+
+  console.log(
+    repositoryName,
+    ownerName,
+    isBranch,
+    branchName,
+    isTag,
+    tagName
+    isRelease,
+    releaseId,
+    payload
+  );
+
+} catch(error) {
+
+  core.setFailed(error.message);
+
+}
