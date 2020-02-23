762d41a6550b392de747ad3bd16a7fa26d9c3ebb
Micah Wood <micah@wpscholar.com>
2020-02-23 09:34:23 -0500

    Test

--- .github/actions/github-event-data/index.js
+++ .github/actions/github-event-data/index.js
@@ -3,8 +3,26 @@ const github = require('@actions/github');
 
 try {
 
+  core.setOutput('string');
+  console.log('Success: string');
+
+  core.setOutput(true);
+  console.log('Success: boolean true');
+
+  core.setOutput(false);
+  console.log('Success: boolean false');
+
+  core.setOutput(1);
+  console.log('Success: integer');
+
+  core.setOutput(null);
+  console.log('Success: null');
+
+  core.setOutput({a: 'b'});
+  console.log('Success: object');
+
   // Get payload
-  const payload = github.context.payload;
+  /*const payload = github.context.payload;
 
   console.log(payload);
 
@@ -30,7 +48,7 @@ try {
   core.setOutput('tagName', tagName);
 
   const releaseId = isRelease ? payload.release.id : '';
-  core.setOutput('releaseId', releaseId);
+  core.setOutput('releaseId', releaseId);*/
 
 } catch(error) {
 
