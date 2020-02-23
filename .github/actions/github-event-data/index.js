80422cc9d15fff92ff8dda338a93b8ec35510593
Micah Wood <micah@wpscholar.com>
2020-02-23 09:36:22 -0500

    What now?

--- .github/actions/github-event-data/index.js
+++ .github/actions/github-event-data/index.js
@@ -3,28 +3,8 @@ const github = require('@actions/github');
 
 try {
 
-  core.setOutput('string');
-  console.log('Success: string');
-
-  core.setOutput(true);
-  console.log('Success: boolean true');
-
-  core.setOutput(false);
-  console.log('Success: boolean false');
-
-  core.setOutput(1);
-  console.log('Success: integer');
-
-  core.setOutput(null);
-  console.log('Success: null');
-
-  core.setOutput({a: 'b'});
-  console.log('Success: object');
-
   // Get payload
-  /*const payload = github.context.payload;
-
-  console.log(payload);
+  const payload = github.context.payload;
 
   const repositoryName = github.context.payload.repository.name;
   core.setOutput('repositoryName', repositoryName);
@@ -33,13 +13,10 @@ try {
   core.setOutput('ownerName', ownerName);
 
   const isBranch = payload.hasOwnProperty('ref') && payload.ref.includes('heads');
-  core.setOutput('isBranch', isBranch);
 
-  const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');
-  core.setOutput('isTag', isTag);
+  const isTag = payload.hasOwnProperty('ref') && payload.ref.includes('tags');  core.setOutput('isTag', isTag);
 
   const isRelease = payload.hasOwnProperty('release');
-  core.setOutput('isRelease', isRelease);
 
   const branchName = isBranch ? payload.ref.substr('refs/tags/'.length, payload.ref.length) : '';
   core.setOutput('branchName', branchName);
@@ -48,7 +25,7 @@ try {
   core.setOutput('tagName', tagName);
 
   const releaseId = isRelease ? payload.release.id : '';
-  core.setOutput('releaseId', releaseId);*/
+  core.setOutput('releaseId', releaseId);
 
 } catch(error) {
 
