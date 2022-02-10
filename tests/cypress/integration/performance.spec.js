e8ca84cd79ad985c064976e55533e605edf2d42a
Micah Wood <micah@wpscholar.com>
2022-02-10 09:41:13 -0500

    Implement Cypress performance tests

---
+++ tests/cypress/integration/performance.spec.js
@@ -0,0 +1,47 @@
+// <reference types="Cypress" />
+
+describe('WordPress', () => {
+
+	before(() => {
+		cy.login(Cypress.env('wpUsername'), Cypress.env('wpPassword'));
+	});
+
+	it('measures page load', () => {
+
+			cy.fixture('performance')
+				.then(
+					(entries) => {
+						entries
+							.forEach(
+								({url, description, loadTimeInSeconds }) => {
+
+									cy.visit(
+										url,
+										{
+											onBeforeLoad: (win) => {
+												win.performance.mark('start-loading');
+											},
+											onLoad: (win) => {
+												win.performance.mark('end-loading');
+											}
+										}
+									)
+										.its('performance')
+										.then((performance) => {
+											performance.measure('pageLoad', 'start-loading', 'end-loading');
+											const measure = performance.getEntriesByName('pageLoad')[0];
+											const duration = measure.duration;
+											assert.isAtMost(duration, loadTimeInSeconds);
+
+											cy.log(
+												`[PERFORMANCE] Page load duration for ${ description }: ${ duration / 1000 } seconds`
+											);
+										});
+								});
+
+					}
+				);
+		}
+	);
+
+});
