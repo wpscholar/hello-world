d82bac137cdf840f2b625c403897cba5bf3b0008
Micah Wood <micah@wpscholar.com>
2022-07-28 15:50:42 -0400

    Update version number

--- hello-world.php
+++ hello-world.php
@@ -13,7 +13,7 @@
  * Plugin URI:
  * Update URI:        hello-world
  * Description:       The world's most amazing and comprehensive WordPress greeting plugin.
- * Version:           1.0
+ * Version:           1.2.4
  * Requires PHP:      5.6
  * Requires at least: 5.0
  * Author:            Micah Wood
