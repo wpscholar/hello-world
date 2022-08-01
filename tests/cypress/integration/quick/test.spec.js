b2b6c5bf3343798791ae79b9635e96b6aa5a0af0
Micah Wood <micah@wpscholar.com>
2022-08-01 19:55:47 -0400

    Fix spec to properly check WP version

--- tests/cypress/integration/quick/test.spec.js
+++ tests/cypress/integration/quick/test.spec.js
@@ -13,7 +13,7 @@ describe('Page', () => {
 		cy.visit('/wp-admin/site-health.php?tab=debug');
 
 		cy.get('#health-check-accordion-block-wp-core').prev().click();
-		cy.get('#health-check-accordion-block-wp-core').find('tr').first().find('td').last().contains(Cypress.env('wpVersion'));
+		cy.get('#health-check-accordion-block-wp-core').find('tr').first().find('td').last().contains(new RegExp(`^${Cypress.env('wpVersion')}`));
 
 		cy.get('#health-check-accordion-block-wp-server').prev().click();
 		cy.get('#health-check-accordion-block-wp-server').find('tr').eq(2).find('td').last().contains(Cypress.env('phpVersion'));
