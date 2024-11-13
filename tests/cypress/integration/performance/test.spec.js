1d6f0624cfbbf1e117882d85f1b68d9a09020a9c
Micah Wood <micah@wpscholar.com>
2024-11-13 14:39:19 -0500

    Create test.spec.js

---
+++ tests/cypress/integration/performance/test.spec.js
@@ -0,0 +1,47 @@
+// <reference types="Cypress" />
+
+describe('WordPress', () => {
+
+	before(() => {
+		cy.login(Cypress.env('WP_USERNAME'), Cypress.env('WP_PASSWORD'));
+	});
+
+	it('measures page load', () => {
+
+			cy.fixture('performance')
+				.then(
+					(entries) => {
+						entries
+							.forEach(
+								({url, description, duration: loadTimeInMilliseconds }) => {
+
+									cy.visit(
+										url,
+										{
+											onBeforeLoad: (win) => {
+												win.performance.mark('start-loading');
+											},
+											onLoad: (win) => {
+												win.performance.mark('end-loading');
+											}
+										}
+									)
+										.its('performance')
+										.then((performance) => {
+											performance.measure('pageLoad', 'start-loading', 'end-loading');
+											const measure = performance.getEntriesByName('pageLoad')[0];
+											const duration = measure.duration;
+											assert.isAtMost(duration, loadTimeInMilliseconds);
+
+											cy.log(
+												`[PERFORMANCE] Page load duration for ${ description }: ${ duration / 1000 } seconds`
+											);
+										});
+								});
+
+					}
+				);
+		}
+	);
+
+});
